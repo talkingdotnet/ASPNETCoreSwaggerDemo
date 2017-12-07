53d8fe01b14def7d22797f9e0594ceeb94a450c5
talkingdotnet <talkingdotnet@gmail.com>
2017-12-07 11:16:16 +0530

    Initial commit

--- ASPNETCoreSwaggerDemo/Controllers/ValuesController.cs
+++ ASPNETCoreSwaggerDemo/Controllers/ValuesController.cs
@@ -9,6 +9,10 @@ namespace ASPNETCoreSwaggerDemo.Controllers
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
+        /// <summary>
+        /// Get API Values.
+        /// </summary>
+        /// <returns></returns>
         // GET api/values
         [HttpGet]
         public IEnumerable<string> Get()
@@ -17,13 +21,23 @@ namespace ASPNETCoreSwaggerDemo.Controllers
         }
 
         // GET api/values/5
+        /// <summary>
+        /// Get API values by ID
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="type">Type of Value</param>
+        /// <returns></returns>
         [HttpGet("{id}")]
-        public string Get(int id)
+        public string Get(int id, ValueType type)
         {
             return "value";
         }
 
         // POST api/values
+        /// <summary>
+        /// Post API Value
+        /// </summary>
+        /// <param name="value"></param>
         [HttpPost]
         public void Post([FromBody]string value)
         {
@@ -34,11 +48,21 @@ namespace ASPNETCoreSwaggerDemo.Controllers
         public void Put(int id, [FromBody]string value)
         {
         }
-
         // DELETE api/values/5
+        /// <summary>
+        /// Delete API Value
+        /// </summary>
+        /// <remarks>This API will delete the values.</remarks>
+        /// <param name="id"></param>
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+        }
+
+        public enum ValueType
+        {
+            Number,
+            Text
         }
     }
 }
--- ASPNETCoreSwaggerDemo/Startup.cs
+++ ASPNETCoreSwaggerDemo/Startup.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Microsoft.Extensions.PlatformAbstractions;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace ASPNETCoreSwaggerDemo
 {
@@ -24,6 +20,28 @@ namespace ASPNETCoreSwaggerDemo
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "My API",
+                    Description = "My First ASP.NET Core Web API",
+                    TermsOfService = "None",
+                    Contact = new Contact() { Name = "Talking Dotnet", Email = "contact@talkingdotnet.com", Url = "www.talkingdotnet.com" }
+                });
+
+                c.SwaggerDoc("v2", new Info
+                {
+                    Version = "v2",
+                    Title = "New API",
+                    Description = "Sample Web API",
+                    TermsOfService = "None",
+                    Contact = new Contact() { Name = "Talking Dotnet", Email = "contact@talkingdotnet.com", Url = "www.talkingdotnet.com" }
+                });
+                c.IncludeXmlComments(GetXmlCommentsPath());
+                c.DescribeAllEnumsAsStrings();
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +53,19 @@ namespace ASPNETCoreSwaggerDemo
             }
 
             app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.DocumentTitle("My API UI");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.SwaggerEndpoint("/swagger/v2/swagger.json", "My API V2");
+            });
+        }
+
+        private string GetXmlCommentsPath()
+        {
+            var app = PlatformServices.Default.Application;
+            return System.IO.Path.Combine(app.ApplicationBasePath, "ASPNETCoreSwaggerDemo.xml");
         }
     }
 }
